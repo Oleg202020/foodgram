3350d166b7797c0ca81dc65a17816a2258e89816
Oleg Larionov <jktu202020@gmail.com>
2025-02-19 22:28:20 +0300

    настраиваю доступ прячу адреса

--- backend/foodgram_main/settings.py
+++ backend/foodgram_main/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(", ")
+ALLOWED_HOSTS = ['158.160.12.176', '127.0.0.1', 'localhost', 'foodgramlar.viewdns.net']
 
 
 # Application definition
