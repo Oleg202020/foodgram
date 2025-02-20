bf2302427cd162b6f822537a0256613a354f7207
Oleg Larionov <jktu202020@gmail.com>
2025-02-20 11:09:02 +0300

    перед первым ревью

--- backend/foodgram_main/settings.py
+++ backend/foodgram_main/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False')
 
-ALLOWED_HOSTS = ['158.160.12.176', '127.0.0.1', 'localhost', 'foodgramlar.viewdns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(', ')
 
 
 # Application definition
