6b50c73c9ed48df077d6f7921fa01f27f2889e68
Oleg Larionov <jktu202020@gmail.com>
2025-02-24 03:46:05 +0300

    третье ревью

--- backend/foodgram_app/urls.py
+++ backend/foodgram_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from foodgram_app.models import RecipeShortLinkView
 
 urlpatterns = [
-    path('s/<str:short_link>/',
+    path('<str:short_link>/',
          RecipeShortLinkView.as_view(), name='recipe_short_link'),
 ]
